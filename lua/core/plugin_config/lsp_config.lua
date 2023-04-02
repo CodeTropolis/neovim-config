5325ee3108bbee5d977c61a73292784b7903b95b
Michael <michael@codetropolis.com>
2023-04-02 14:13:15 -0500

    Revise keymaps in lsp_config.lua.  The keymap for format included l which was making using l normally sluggish.

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -10,8 +10,7 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'lr', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<space>lf', function() vim.lsp.buf.format { async = true } end, bufopts)
+  vim.keymap.set('n', '<C-f>', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -51,6 +50,3 @@ require('lspconfig').emmet_ls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
 }
-
-
-
