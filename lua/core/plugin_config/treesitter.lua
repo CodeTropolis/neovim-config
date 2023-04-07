217ea8c524d07b0b981506e50905c254dc80cc74
Michael <michael@codetropolis.com>
2023-04-07 05:38:46 -0500

    Remove which_key plugin.

--- lua/core/plugin_config/treesitter.lua
+++ lua/core/plugin_config/treesitter.lua
@@ -6,7 +6,7 @@ require 'nvim-treesitter.configs'.setup {
   --  Since 0.8.0, Neovim bundles parsers and queries for c, lua, vim, and help.
   --  If you use nvim-treesitter, you must make sure these parsers are 
   --  installed via nvim-treesitter so that both parser and queries (which are always installed) are taken from nvim-treesitter.
-  ensure_installed = {'typescript', 'css', 'scss', 'html', 'javascript', 'c', 'lua', 'vim', 'help'},
+  ensure_installed = {'typescript', 'css', 'scss', 'html', 'javascript', 'c', 'lua', 'vim' },
 
   sync_install = false,
   auto_install = true,
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -81,19 +81,17 @@ return require('packer').startup(function(use)
     config = function() require("nvim-autopairs").setup {} end
   }
 
-use {
-  "folke/which-key.nvim",
-  config = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-    require("which-key").setup {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
-  end
-}
-
+--use {
+--  "folke/which-key.nvim",
+--  config = function()
+--    vim.o.timeout = true
+--    vim.o.timeoutlen = 300
+--    require("which-key").setup {
+--      -- your configuration comes here
+--      -- or leave it empty to use the default settings
+--    }
+--  end
+--}
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
