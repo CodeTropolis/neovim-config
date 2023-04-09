2984d3a04e1ce832ac195919db338975c33109cf
Michael <michael@codetropolis.com>
2023-04-08 22:21:29 -0500

    Added line to create a conflict.

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -81,7 +81,7 @@ return require('packer').startup(function(use)
     "windwp/nvim-autopairs",
     config = function() require("nvim-autopairs").setup {} end
   }
-
+-- Confilct b
 use {'akinsho/git-conflict.nvim', tag = "*", config = function()
   require('git-conflict').setup()
 end}
