a90f6e5c289544fb87d9b6f20fa121d4c1ae2ec9
Michael <michael@codetropolis.com>
2023-04-08 22:14:56 -0500

    Removed commented out plugin.

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -82,18 +82,6 @@ return require('packer').startup(function(use)
     config = function() require("nvim-autopairs").setup {} end
   }
 
---use {
---  "folke/which-key.nvim",
---  config = function()
---    vim.o.timeout = true
---    vim.o.timeoutlen = 300
---    require("which-key").setup {
---      -- your configuration comes here
---      -- or leave it empty to use the default settings
---    }
---  end
---}
---
 use {'akinsho/git-conflict.nvim', tag = "*", config = function()
   require('git-conflict').setup()
 end}
