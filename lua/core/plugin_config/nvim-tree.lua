402b79f2a7a6bc352962971efdfa0b3d89243e9f
Michael <michael@codetropolis.com>
2023-12-18 19:28:58 -0600

    Minor code formatting in nvim-tree.lua.

--- lua/core/plugin_config/nvim-tree.lua
+++ lua/core/plugin_config/nvim-tree.lua
@@ -3,7 +3,7 @@ vim.gloaded_netrwPlugin = 1
 
 require('nvim-tree').setup({
   view = {
-  side = "left",
+   side = "left",
   width = 30,
   },
 })
@@ -11,9 +11,8 @@ require('nvim-tree').setup({
 -- Find current file and open dir in NvimTree
 vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
 
--- Open NvimTree on enter
+-- Open NvimTree on NeoVim launch
 local function open_nvim_tree()
-  -- open the tree
   require("nvim-tree.api").tree.open()
 end
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
