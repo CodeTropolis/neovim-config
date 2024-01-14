b6649af5bea3cb36b9285d56c05656101b176c8c
Michael <michael@codetropolis.com>
2024-01-13 23:41:10 -0600

    Added keymaps.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,3 +9,22 @@ vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 -- Upon Ctrl+V, move selection up and down.
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Move cursor to split on right.
+vim.keymap.set("n", "<leader>m", "<C-w><right>")
+
+-- Move cursor to split on left.
+vim.keymap.set("n", "<leader>p", "<C-w><left>")
+
+-- Move cursor to split above.
+vim.keymap.set("n", "<leader>u", "<C-w><up>")
+
+-- Move cursor to split below.
+vim.keymap.set("n", "<leader>b", "<C-w><down>")
+
+-- Set terminal to normal mode.
+-- Do this prior to attempting to split terminal.
+--vim.keymap.set("t", "<C-e>", "<C-\\><C-n>")
+
+---- Splt Terminal horizontally
+--vim.keymap.set("n", "sth", ":split<Return><C-w>w :terminal<CR>")
