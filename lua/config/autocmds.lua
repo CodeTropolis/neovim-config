ede947a0c57411b4045eaa30c8a615b102518458
Michael <michael@codetropolis.com>
2024-01-15 05:41:05 -0600

    Added autocommands for opening terminal and then splitting it verically.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,18 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Open Terminal upon launching Neovim.
+vim.api.nvim_exec(
+  [[
+  autocmd VimEnter * call timer_start(50, {-> execute('ToggleTerm')})
+]],
+  false
+)
+
+vim.api.nvim_exec(
+  [[
+  autocmd VimEnter * call timer_start(100, {-> execute(':vsplit | term')})
+]],
+  false
+)
